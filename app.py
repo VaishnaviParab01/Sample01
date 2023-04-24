fa26306775ba6b20632059ffcdc79da93f621533
Vaishnavi Parab <68656329+VaishnaviParab01@users.noreply.github.com>
2023-04-24 20:03:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,44 @@
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_text as text
+import pandas as pd
+import numpy as np
+
 import os
+from io import StringIO
+
+import re
+import string
 
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory,Response, url_for)
 
 app = Flask(__name__)
 
+# Lab = {0: 'Bank Charges and Fees', 1: 'Groceries', 2: 'Transport and Fuel', 3: 'Cellphone',
+       # 4: 'Restaurants and Take-Aways', 5: 'Entertainment', 6: 'Internet and Telephone', 7: 'Holidays and Travel',
+       # 8: 'Clothing', 9:'Gambling'}
+
+# list_10cat = ['Bank Charges and Fees', 'Groceries', 'Transport and Fuel', 'Cellphone', 'Restaurants and Take-Aways', 
+              # 'Entertainment', 'Internet and Telephone', 'Holidays and Travel', 'Clothing', 'Gambling']
+
+# def semi_clean(text):
+    # final_string = ""
+    # text = text.lower()
+    # text = re.sub(r'\n', '', text)
+    # translator = str.maketrans('.', ' ')
+    # text = text.translate(translator)
+    # translator = str.maketrans('', '', string.punctuation)
+    # text = text.translate(translator)
+    # final_string = text
+    # return final_string
+# def predict(model, data):
+    # pred =  model.predict(data)
+    # return list(map(lambda x: (Lab[x.argmax()], x.max()), pred))
+# def get_categories(data):
+    # pred_data = pd.DataFrame(np.array(predict(LoadModel_TF02, data)))
+    # pred_data.columns = ["Prediction Category", "Prediction Probability"]
+    # return pred_data
 
 @app.route('/')
 def index():
@@ -19,14 +53,38 @@ def favicon():
 @app.route('/classifyresult', methods=['POST'])
 def classify():
    file = request.files['file']
-
+   result = ""
    if file:
-       file_content = file.read()
+       # # TODO: Classify Content and return Result Content as CSV FIle
+       df = pd.read_csv(file)  # file.file
 
-       #TODO: Classify Content and return Result Content as CSV FIle
+       # df['Description_New'] = df['Description'].apply(lambda x: semi_clean(x))
+       # df["Combo"] = np.where((df["Reference"].notnull()) & (df["Description"] != df["Reference"]) & (df["Reference"].str.isnumeric() == False), df["Description"] + ' ' + df["Reference"], df["Description"])
+       # df.drop(columns=['Description','Reference'], axis=1, inplace = True)
+       # df.rename(columns={'Combo':'Description'}, inplace=True)
+       # df = df.loc[df["CategoryDescription"].isin(list_10cat)]
+       # data = df["Description_New"]
+       
+       try:
+           # preprocessor = hub.KerasLayer("universal-sentence-encoder-cmlm_multilingual-preprocess_2")
+           # LoadModel_TF02 = tf.keras.models.load_model('azureml://locations/centralindia/workspaces/d40c591a-a486-4c85-aa2c-e6ded75203de/models/TFModel02/versions/1', compile=False, custom_objects={'KerasLayer':preprocessor})
+           
+           # pred_val = get_categories(data)
+           # print(pred_val)
+           
+           # Pred_Data = pd.concat([df,pred_val], axis = 1)
+           # Pred_Data = Pred_Data.drop(Pred_Data.columns[0],axis=1)
+           # print(Pred_Data)
+           # s = StringIO()
+           # Pred_Data.to_csv(s, index=False)
 
-       print('Request for classification received as file name=%s' % file.filename)
-       return Response(file_content,mimetype='text/csv')
+           print('Request for classification received as file name=%s' % file.filename)
+           result = df #s.getvalue()
+       except Exception as e: # work on python 3.x
+            result = str(e)
+       return Response(result, mimetype='text/csv')
+  
+       
    else:
        print('Request for classification received without file -- redirecting')
        return redirect(url_for('index'))
